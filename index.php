39a7db3cc4e5e7e3597ddb65cd37d3928448413a
Chris Thomas <chris.thomas94@hotmail.co.uk>
2016-02-22 22:51:40 +0000

    horey shit it's working

--- index.php
+++ index.php
@@ -16,45 +16,86 @@ require_once 'include/DB_Functions.php';
 $db = new DB_Functions();
 
 $inputJSON = file_get_contents('php://input');
-echo $inputJSON;
 
 $filename="test.txt";
-//file_put_contents($filename, $inputJSON);
+//file_put_contents($filename,json_decode(file_get_contents('php://input'), true));
+file_put_contents($filename, $inputJSON);
 
-//$_POST = json_decode(file_get_contents('php://input'), true);
+//die(json_decode($inputJSON, true));
 
-$method = $_SERVER['REQUEST_METHOD'];
-file_put_contents($filename, $method);
+$decoded = json_decode(file_get_contents('php://input'), true);
 
-
-$name = (isset($_POST['name']) ? $_POST['name'] : null);
-$email = (isset($_POST['email']) ? $_POST['email'] : null);
-$password = (isset($_POST['password']) ? $_POST['password'] : null);
-  
-// check if user is already existed
-if ($db->isUserExisted($email)) {
-	// user is already existed - error response
-	$response["error"] = TRUE;
-	$response["error_msg"] = "User already existed";
-	echo json_encode($response);
+if (isset($decoded['tag']) && !empty($decoded['tag'])) {
+    // get tag
+    $tag = $decoded['tag'];
+ 
+    // response Array
+    $response = array("tag" => $tag, "error" => FALSE);
+ 
+    // checking tag
+    if ($tag == 'login') {
+        // Request type is check Login
+        $email = (isset($decoded['email']) ? $decoded['email'] : null);
+		$password = (isset($decoded['password']) ? $decoded['password'] : null);
+ 
+        // check for user
+        $user = $db->getUserByEmailAndPassword($email, $password);
+        if ($user != false) {
+            // user found
+            $response["error"] = FALSE;
+            $response["uid"] = $user["unique_id"];
+            $response["user"]["name"] = $user["name"];
+            $response["user"]["email"] = $user["email"];
+            $response["user"]["created_at"] = $user["created_at"];
+            $response["user"]["updated_at"] = $user["updated_at"];
+            echo json_encode($response);
+        } else {
+            // user not found
+            // echo json with error = 1
+            $response["error"] = TRUE;
+            $response["error_msg"] = "Incorrect email or password!";
+            echo json_encode($response);
+        }
+    } else if ($tag == 'register') {
+        // Request type is Register new user
+		$name = (isset($decoded['name']) ? $decoded['name'] : null);
+		$email = (isset($decoded['email']) ? $decoded['email'] : null);
+		$password = (isset($decoded['password']) ? $decoded['password'] : null);
+ 
+        // check if user is already existed
+        if ($db->isUserExisted($email)) {
+            // user is already existed - error response
+            $response["error"] = TRUE;
+            $response["error_msg"] = "User already existed";
+            echo json_encode($response);
+        } else {
+            // store user
+            $user = $db->storeUser($name, $email, $password);
+            if ($user) {
+                // user stored successfully
+                $response["error"] = FALSE;
+                $response["uid"] = $user["unique_id"];
+                $response["user"]["name"] = $user["name"];
+                $response["user"]["email"] = $user["email"];
+                $response["user"]["created_at"] = $user["created_at"];
+                $response["user"]["updated_at"] = $user["updated_at"];
+                echo json_encode($response);
+            } else {
+                // user failed to store
+                $response["error"] = TRUE;
+                $response["error_msg"] = "Error occured in Registartion";
+                echo json_encode($response);
+            }
+        }
+    } else {
+        // user failed to store
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Unknown 'tag' value. It should be either 'login' or 'register'";
+        echo json_encode($response);
+    }
 } else {
-	// store user
-	$user = $db->storeUser($name, $email, $password);
-	if ($user) {
-		// user stored successfully
-		$response["error"] = FALSE;
-		$response["uid"] = $user["unique_id"];
-		$response["user"]["name"] = $user["name"];
-		$response["user"]["email"] = $user["email"];
-		$response["user"]["created_at"] = $user["created_at"];
-		$response["user"]["updated_at"] = $user["updated_at"];
-		echo json_encode($response);
-	} else {
-		// user failed to store
-		$response["error"] = TRUE;
-		$response["error_msg"] = "Error occured in Registartion";
-		echo json_encode($response);
-	}
+    $response["error"] = TRUE;
+    $response["error_msg"] = "Required parameter 'tag' is missing!";
+    echo json_encode($response);
 }
-
 ?>
